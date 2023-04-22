1ed6b319e66db26392c8f8fd5683a5fecf391211
minjyo <minjyo8823@gmail.com>
2023-04-22 15:56:24 +0900

    feat: 메인 페이지 기본 화면 마크업 적용, 기본 버튼 클릭 함수 추가, tailwindcss 속성 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,22 +10,32 @@ module.exports = {
     ],
     theme: {
         colors: {
-            purple: '#5D31FE',
-            orange: '#FF5416',
-            green: '#DEF048',
-            black: '#222222',
-            white: '#FFFFFF',
-            gray: '#969696',
-            hr: '#C8C8C8',
-            error: '#E83459',
+            'purple': '#5D31FE',
+            'orange': '#FF5416',
+            'green': '#DEF048',
+            'black': '#222222',
+            'white': '#FFFFFF',
+            'gray': '#969696',
+            'light-gray': '#787878',
+            'hr': '#C8C8C8',
+            'error': '#E83459',
         },
         //spacing: default (https://tailwindcss.com/docs/customizing-spacing#default-spacing-scale)
         borderRadius: {
             none: '0',
-            DEFAULT: '16px',
+            DEFAULT: '6px',
         },
-
-        extend: {},
+        fontSize: {
+            'sm': '0.7rem', // 14px
+            'base': '0.8rem', // 16px
+            'xl': '1.01rem', // 20px
+            '2xl': '1.41rem', // 24px
+        },
+        extend: {
+            boxShadow: {
+                md: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+            },
+        },
     },
     plugins: [],
 };
