a68fedd0c78cbbb7f86aea0ee07331fe75051703
Derek <ghdtjrrl94@gmail.com>
2023-04-23 19:51:18 +0900

    feat: GNB 작성
    - join, join-welcome, login 에서는 GNB 미노출 처리 요망
    - 앞선 MR 머지되면 작업 예정.

--- components/Header.vue
+++ components/Header.vue
@@ -1,17 +1,28 @@
+<script setup lang="ts">
+import {useRouter} from 'nuxt/app';
+
+import ThreePickLogo from '~/assets/svg/ThreePickLogo.svg?component';
+
+const router = useRouter();
+
+const onClickLogo = () => {
+    router.push('/');
+};
+</script>
 <template>
     <nav>
-        <ul class="flex flex-row">
-            <li class="font-bold mr-1"><NuxtLink to="/">Home</NuxtLink></li>
-            <li class="font-bold mr-1">
-                <NuxtLink to="/login">Login</NuxtLink>
-            </li>
-            <li class="font-bold mr-1">
-                <NuxtLink to="/reward">Reward</NuxtLink>
-            </li>
-            <li class="font-bold mr-1">
-                <NuxtLink to="/my">My Page</NuxtLink>
-            </li>
+        <ul
+            class="flex items-center max-w-[988px] py-[25px] justify-between m-auto sm:px-4"
+        >
+            <ThreePickLogo class="cursor-pointer" @click="onClickLogo" />
+            <div class="flex">
+                <NuxtLink class="font-[500] text-base" to="/reward"
+                    >리워드</NuxtLink
+                >
+                <NuxtLink class="ml-[60px] font-[500] text-base" to="/my"
+                    >마이 페이지</NuxtLink
+                >
+            </div>
         </ul>
-        <hr />
     </nav>
 </template>
