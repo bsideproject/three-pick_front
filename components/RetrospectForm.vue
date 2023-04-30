f96c67049c3170fcab0c3ba9c61d02f51afb186a
minjyo <minjyo8823@gmail.com>
2023-04-30 19:21:28 +0900

    feat: 회고 폼에 취소/생성 버튼 추가, Basic Button에 border color 추가

--- components/RetrospectForm.vue
+++ components/RetrospectForm.vue
@@ -2,6 +2,8 @@
 import {ref} from 'vue';
 
 const content = ref('');
+
+const emit = defineEmits(['onConfirm', 'onCancel']);
 </script>
 
 <template>
@@ -9,4 +11,12 @@ const content = ref('');
         class="border-[1px] border-gray50 rounded h-[172px] p-4 focus:border-black resize-none outline-none"
         :value="content"
     />
+    <div class="flex flex-row gap-2 mt-4 text-base">
+        <basic-button :theme="'ghost'" @onClick="emit('onCancel')"
+            >취소하기</basic-button
+        >
+        <basic-button :theme="'primary'" @onClick="emit('onConfirm')"
+            >생성하기</basic-button
+        >
+    </div>
 </template>
