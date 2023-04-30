fab7f05c76bc3bd2b9d03cfec98707558049e2ba
minjyo <minjyo8823@gmail.com>
2023-04-30 21:07:56 +0900

    feat: 회고 api 추가

---
+++ apis/retrospect.ts
@@ -0,0 +1,43 @@
+import type {GoalType, GoalStatus} from '~/types';
+
+/**
+ * 회고 생성
+ */
+export const createRetrospectApi = (
+    accountId: number,
+    content: string,
+    retrospectDate: string,
+) =>
+    useApi(`/api/retrospects`, {
+        method: 'POST',
+        body: {
+            accountId,
+            content,
+            retrospectDate,
+        },
+    });
+
+/**
+ * 목표 수정
+ */
+export const updateRetrospectApi = (
+    accountId: number,
+    content: string,
+    retrospectId: number,
+) =>
+    useApi(`/api/retrospects`, {
+        method: 'PUT',
+        body: {
+            accountId,
+            content,
+            retrospectId,
+        },
+    });
+
+/**
+ * 회고 조회
+ */
+export const getRetrospectApi = (accountId: number, date: string) =>
+    useApi(`/api/retrospects`, {
+        method: 'GET',
+    });
