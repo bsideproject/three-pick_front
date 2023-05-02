1f09a94eff79f0ed177cea1772e87cbebecc15a3
derek <52649378+Derek-94@users.noreply.github.com>
2023-05-02 23:39:21 +0900

    feat: useCookie로 쿠키관리 (#17)

--- stores/AuthStore.ts
+++ stores/AuthStore.ts
@@ -3,22 +3,32 @@ export const useAuthStore = defineStore('auth', () => {
     const refreshToken = ref<string>('');
     const accountId = ref<number>(0);
 
+    const accessTokenCookie = useCookie<string>('accessToken');
+    const refreshTokenCookie = useCookie<string>('refreshToken');
+    const accountIdCookie = useCookie<number>('accountId');
+
     function setAccessToken(auth: string) {
         accessToken.value = auth;
+        accessTokenCookie.value = auth;
     }
 
     function setRefreshToken(refresh: string) {
         refreshToken.value = refresh;
+        refreshTokenCookie.value = refresh;
     }
 
     function setAccountId(id: number) {
         accountId.value = id;
+        accountIdCookie.value = id;
     }
 
     return {
         accessToken,
         refreshToken,
         accountId,
+        accessTokenCookie,
+        refreshTokenCookie,
+        accountIdCookie,
         setAccessToken,
         setRefreshToken,
         setAccountId,
